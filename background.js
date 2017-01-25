d21f43d98d67f23920a65d003cdcbc6198703ec6
Ivan Novikov <ivan7237d@gmail.com>
2017-01-25 15:00:05 +0300

    Add ability to submit pages

--- background.js
+++ background.js
@@ -44,13 +44,18 @@ chrome.tabs.onUpdated.addListener(async(tabId, changeInfo, tab) => {
   }
   delete commentUrls[tabId];
   chrome.browserAction.setBadgeText({text: '', tabId});
+  chrome.browserAction.setTitle({title: 'Checking for HN comments...', tabId});
   url = stripHash(url);
   let comments = await getHNComments(url);
   if (!comments) {
+    chrome.browserAction.setTitle(
+        {title: 'No HN comments. Click to submit this page.', tabId});
     return;
   }
   chrome.browserAction.setBadgeText(
       {text: comments.num_comments.toString(), tabId});
+  chrome.browserAction.setTitle(
+      {title: 'Click to open HN comments in a new tab.', tabId});
   commentUrls[tabId] =
       `https://news.ycombinator.com/item?id=${comments.objectID}`;
 });
@@ -59,5 +64,10 @@ chrome.browserAction.onClicked.addListener(async currentTab => {
   const commentUrl = commentUrls[currentTab.id];
   if (commentUrl) {
     chrome.tabs.create({url: commentUrl});
+  } else {
+    chrome.tabs.create({
+      url: `http://news.ycombinator.com/submitlink?u=${encodeURIComponent(
+          currentTab.url)}&t=${encodeURIComponent(currentTab.title)}`
+    })
   }
 });
